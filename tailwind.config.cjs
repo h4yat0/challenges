f3866cf30e76bbe689d2e7e3c8229aab6a856ee5
h4yat0 <63439982+h4yat0@users.noreply.github.com>
2023-01-28 17:59:12 -0300

    [add] tailwind setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,17 @@
 module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		fontFamily: {
+			roboto: ["Roboto, sans-serif"],
+			inter: ["Inter, sans-serif"],
+		},
+		extend: {
+			colors: {
+				background: "#0B0911",
+				testeBackground: "#130F1E",
+				grid: "#1c162c",
+			},
+		},
 	},
 	plugins: [],
 };
